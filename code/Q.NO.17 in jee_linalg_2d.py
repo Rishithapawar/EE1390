a298d23408297f41e3516a642fda3f50f4c77cca
Rishithapawar <47626095+Rishithapawar@users.noreply.github.com>
2019-02-15 09:33:16 +0530

    Update Q.NO.17 in jee_linalg_2d.py

--- code/Q.NO.17 in jee_linalg_2d.py
+++ code/Q.NO.17 in jee_linalg_2d.py
@@ -1,11 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x=np.linspace(-10,10,10000)
-y1=np.sqrt(2.25*((x**2)/9-1))
-y2=-np.sqrt(2.25*((x**2)/9-1))
-plt.plot(x,y1)
-plt.plot(x,y2)
+t=np.linspace(0,2,100)
+n1=np.zeros((2,100))
+n2=np.zeros((2,100))
+p=np.zeros((2,100))
+
+for i in range(100):
+  n1[0,i]=t[i]
+  n2[0,i]=1
+  p[0,i]=3*(t[i])
+  n1[1,i]=2
+  n2[1,i]=2*(t[i])
+  p[1,i]=3
+  t1=np.array([n1[0,i],n1[1,i]])
+  t2=np.array([n2[0,i],n2[1,i]])
+  P=np.array([p[0,i],p[1,i]])
+
+A=np.vstack([t1,t2]).T
+x=np.matmul(np.linalg.inv(A),P)
+
+plt.plot(x[0],x[1],'o')
 plt.grid()
-plt.ylabel('$y$')
-plt.xlabel('$x$')
+plt.axis('equal')
 plt.show()
